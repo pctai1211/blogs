960869bbe58221a6f09d12e083d50a130af85614
taiphan-tnm <taiphan@tenomad.com>
2025-01-16 08:22:30 +0700

    init source

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Header from '@/components/Header';
+import ThemeToggle from '@/components/ThemeToggle';
+
+export default async function Index() {
+  return (
+    <div className="flex w-full flex-1 flex-col items-center gap-20">
+      <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
+        <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm"></div>
+      </nav>
+
+      <div className="flex max-w-4xl flex-1 flex-col gap-20 px-3">
+        <Header />
+        <main className="flex flex-1 flex-col gap-6"></main>
+      </div>
+
+      <footer className="w-full justify-center border-t border-t-foreground/10 p-8 text-center text-xs">
+        <p className="mb-6"></p>
+        <ThemeToggle />
+      </footer>
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import NextLogo from './NextLogo';
+export default function Header() {
+  return (
+    <div className="flex flex-col items-center gap-16">
+      <div className="flex items-center justify-center gap-8">
+        <span className="h-6 rotate-45 border-l" />
+        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+          <NextLogo />
+        </a>
+      </div>
+      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
+      <p className="mx-auto max-w-xl text-center text-3xl !leading-tight lg:text-4xl">
+        The fastest way to build apps with{' '}
+        <a href="https://nextjs.org/" target="_blank" className="font-bold hover:underline" rel="noreferrer">
+          Next.js
+        </a>
+      </p>
+      <div className="my-8 w-full bg-gradient-to-r from-transparent via-foreground/10 to-transparent p-[1px]" />
+    </div>
+  );
+}
